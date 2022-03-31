df7b85223ceae3c447cba6ba3dabf2a75789c95c
김시원 <kimsea13@naver.com>
2022-03-31 18:02:19 +0900

    helpdesk 키워드 추출 2차 프로토타입 및 3차 진행중

--- src/com/company/HelpDeskKeyword.java
+++ src/com/company/HelpDeskKeyword.java
@@ -14,6 +14,7 @@ public class HelpDeskKeyword {
         String path = "C:\\Users\\urp시스템\\Desktop\\helpDesk 분석\\";	//파일 경로 설정
         String filename = "HelpDesk 분석_2019_2022_0310.xlsx";	//파일명 설정
         List<List<String>> list = readExcel(path,filename);
+
         createDistinctWordSet(list);
         create7ColumnList(list);
         checkSimilarity(create7ColumnList(list),createDistinctWordSet(list));
@@ -78,38 +79,76 @@ public class HelpDeskKeyword {
                 .map(it -> it.get(7))
                 .collect(Collectors.toList());
 
-        //wordList.stream().forEach(System.out::println);
-        //System.out.println(wordList.size());
 
         Set<String> distinctWordSet = wordList.stream()
                 .map(it -> it.split(" "))
                 .flatMap(Arrays::stream)
                 .map(it -> it.replaceAll("[^\uAC00-\uD7A30-9a-zA-Z//>/]"," "))
+                .map(it -> it.trim())
                 .filter(it -> it.length() >1)
                 //.map(it -> it.substring(it.length()-2,it.length()))
                 .collect(Collectors.toSet());
 
-        //distinctWordSet.stream().forEach(System.out::println);
+        String oneZosa ="이/가/을/를/에";
+        String twoZosa ="이나/이고/이라/이랑/에서/가면/에서/에게/한테/으로";
+        String threeZosa ="이라고/이라는/에서는/에서만/에서는/에서도/한테는/한테도/한테만";
+
+//        if (str.length() <= 1){
+//            if (oneZosa.contains(str)){
+//                distinctWordSet.remove(str);
+//            }
+//        }else if(str.length() <= 2){
+//            if (twoZosa.contains(str)){
+//                distinctWordSet.remove(str);
+//            }else if (oneZosa.contains(str)){
+//                distinctWordSet.remove(str);
+//            }
+//        }else{
+//            if(threeZosa.contains(str.substring(str.length()-3,str.length()))){
+//                distinctWordSet.remove(str);
+//            }else if (twoZosa.contains(str.substring(str.length()-2,str.length()))){
+//                distinctWordSet.remove(str);
+//            }else if (oneZosa.contains(str.substring(str.length()-1,str.length()))){
+//                distinctWordSet.remove(str);
+//            }
+//        }
+
+        for (String str : distinctWordSet){
+            if (str.length() <= 1){
+                if (oneZosa.contains(str)){
+                    distinctWordSet.remove(str);
+                }
+            }else if(str.length() <= 2){
+                if (twoZosa.contains(str.substring(str.length()-2))){
+                    distinctWordSet.remove(str);
+                }else if (oneZosa.contains(str)){
+                    distinctWordSet.remove(str);
+                }
+            }
+        }
+
         return distinctWordSet;
     }
 
     public static List<String> create7ColumnList(List<List<String>> list){
-        List<String> wordList = list.stream()
+        List<String> columnList = list.stream()
                 .map(it -> it.get(7))
                 .collect(Collectors.toList());
-        return wordList;
+
+        //list.stream().map(it -> it.get(7)).forEach(System.out::println);
+        return columnList;
     }
 
-    public static void checkSimilarity(List<String> wordList, Set<String> distinctWordSet){
+    public static void checkSimilarity(List<String> columnList, Set<String> distinctWordSet){
 
         Map<String,Integer> wordSimilarityMap = new HashMap<>();
 
-        //wordSimilarityMap =
-
         for (String str : distinctWordSet) {
             int count = 1;
-            for (int i = 0; i < wordList.size(); i++) {
-                if (wordList.get(i).contains(str)){
+
+            for (int i = 0; i < columnList.size(); i++) {
+                if (columnList.get(i).contains(str)){
+
                     if(wordSimilarityMap.get(str)!=null){
                         count++;
                     }
@@ -118,9 +157,30 @@ public class HelpDeskKeyword {
             }
         }
 
+        //정렬후 list 담기
         wordSimilarityMap.entrySet().stream()
-
+                .sorted(Comparator.comparing(Map.Entry::getKey))
+                //.map(it->it.getKey())
+                //.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
                 .forEach(System.out::println);
 
     }
+
+    @Deprecated
+    public static List<String> makeDictionary(Set<String> distinctWordSet){
+        List<String> includedList = distinctWordSet.stream()
+                //.sorted(Comparator.comparing(String::new))
+                .collect(Collectors.toList());
+
+        for (String strSet : distinctWordSet){
+            for (int i =0 ;i<includedList.size();i++){
+                if (!strSet.equals(includedList.get(i)) && includedList.get(i).contains(strSet)){
+                    includedList.remove(i);
+                }
+            }
+        }
+
+        return includedList;
+    }
+
 }
