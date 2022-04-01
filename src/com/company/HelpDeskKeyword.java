c5894a61c9823af7939eec0a924dcff200be8c22
김시원 <kimsea13@naver.com>
2022-04-01 17:51:44 +0900

    helpdesk 키워드 추출 3차 완료

--- src/com/company/HelpDeskKeyword.java
+++ src/com/company/HelpDeskKeyword.java
@@ -5,20 +5,26 @@ import org.apache.poi.xssf.usermodel.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class HelpDeskKeyword {
-    public static void main(String[] args) {
-        String path = "C:\\Users\\urp시스템\\Desktop\\helpDesk 분석\\";	//파일 경로 설정
-        String filename = "HelpDesk 분석_2019_2022_0310.xlsx";	//파일명 설정
-        List<List<String>> list = readExcel(path,filename);
+    public static String path = "C:\\Users\\urp시스템\\Desktop\\helpDesk 분석\\";	//파일 경로 설정
+    public static String filename = "HelpDesk 분석_2019_2022_0310.xlsx";	//파일명 설정
 
-        createDistinctWordSet(list);
-        create7ColumnList(list);
-        checkSimilarity(create7ColumnList(list),createDistinctWordSet(list));
+    public static List<List<String>> data = readExcel(path,filename);
+
+    public static void main(String[] args) {
+        List<String> column7List =createColumnList(data,7);
+
+        List<Map.Entry<String, Double>> similarityList = checkSimilarity(column7List, initDistinctWordSet(column7List));
+        //similarityList.stream().forEach(System.out::println);
+
+        Map<String,Double> detailOneSimilarityMap = detailSimilarity(column7List,similarityList);
+        detailOneSimilarityMap.entrySet().stream().forEach(System.out::println);
+
     }
+
     public static List<List<String>> readExcel(String path,String filename) {
         List<List<String>> list = new ArrayList<List<String>>();
 
@@ -73,56 +79,69 @@ public class HelpDeskKeyword {
         return list;
     }
 
-    public static Set<String> createDistinctWordSet(List<List<String>> list){
+    public static Set<String> initDistinctWordSet(List<String> columnList){
 
-        List<String> wordList = list.stream()
-                .map(it -> it.get(7))
-                .collect(Collectors.toList());
-
-
-        Set<String> distinctWordSet = wordList.stream()
+        Set<String> distinctWordSet = columnList.stream()
                 .map(it -> it.split(" "))
                 .flatMap(Arrays::stream)
                 .map(it -> it.replaceAll("[^\uAC00-\uD7A30-9a-zA-Z//>/]"," "))
                 .map(it -> it.trim())
-                .filter(it -> it.length() >1)
-                //.map(it -> it.substring(it.length()-2,it.length()))
+                .filter(it -> it.length() > 1)
                 .collect(Collectors.toSet());
 
+        return removeZosa(distinctWordSet);
+    }
+
+    public static Set<String> detailDistinctWordSet(List<String> columnList, String word){
+        Set<String> detailDistinctWordSet = columnList.stream()
+                .map(it -> it.split(" "))
+                .flatMap(Arrays::stream)
+                .map(it -> it.replaceAll("[^\uAC00-\uD7A30-9a-zA-Z//>/]"," "))
+                .map(it -> it.trim())
+                .filter(it->it.contains(word))
+                .collect(Collectors.toSet());
+        return removeZosa(detailDistinctWordSet);
+    }
+
+    public static Set<String> removeZosa(Set<String> distinctWordSet){
+
         String oneZosa ="이/가/을/를/에";
-        String twoZosa ="이나/이고/이라/이랑/에서/가면/에서/에게/한테/으로";
-        String threeZosa ="이라고/이라는/에서는/에서만/에서는/에서도/한테는/한테도/한테만";
+        String twoZosa ="이나/이고/이라/이랑/에서/가면/에서/에게/한테/으로/않음/안됨/되지/";
+        String threeZosa ="이라고/이라는/에서는/에서만/에서는/에서도/한테는/한테도/한테만/됩니다/";
+        String fourZosa="않습니다/나옵니다/";
 
-//        if (str.length() <= 1){
-//            if (oneZosa.contains(str)){
-//                distinctWordSet.remove(str);
-//            }
-//        }else if(str.length() <= 2){
-//            if (twoZosa.contains(str)){
-//                distinctWordSet.remove(str);
-//            }else if (oneZosa.contains(str)){
-//                distinctWordSet.remove(str);
-//            }
-//        }else{
-//            if(threeZosa.contains(str.substring(str.length()-3,str.length()))){
-//                distinctWordSet.remove(str);
-//            }else if (twoZosa.contains(str.substring(str.length()-2,str.length()))){
-//                distinctWordSet.remove(str);
-//            }else if (oneZosa.contains(str.substring(str.length()-1,str.length()))){
-//                distinctWordSet.remove(str);
-//            }
-//        }
 
-        for (String str : distinctWordSet){
-            if (str.length() <= 1){
-                if (oneZosa.contains(str)){
-                    distinctWordSet.remove(str);
+        Iterator<String> iterator = distinctWordSet.iterator();
+
+        while (iterator.hasNext()){
+            String str = iterator.next();
+            if (str.length() <= 1) {
+                if (oneZosa.contains(str)) {
+                    iterator.remove();
                 }
-            }else if(str.length() <= 2){
-                if (twoZosa.contains(str.substring(str.length()-2))){
-                    distinctWordSet.remove(str);
-                }else if (oneZosa.contains(str)){
-                    distinctWordSet.remove(str);
+            } else if (str.length() <= 2) {
+                if (twoZosa.contains(str)) {
+                    iterator.remove();
+                }else if (oneZosa.contains(str.substring(str.length() - 1))){
+                    iterator.remove();
+                }
+            } else if (str.length() <= 3){
+                if(threeZosa.contains(str.substring(str.length()-3))){
+                    iterator.remove();
+                }else if (twoZosa.contains(str.substring(str.length()-2))){
+                    iterator.remove();
+                }else if (oneZosa.contains(str.substring(str.length()-1))){
+                    iterator.remove();
+                }
+            } else {
+                if (fourZosa.contains(str.substring(str.length()-4))){
+                    iterator.remove();
+                }else if(threeZosa.contains(str.substring(str.length()-3))){
+                    iterator.remove();
+                }else if (twoZosa.contains(str.substring(str.length()-2))){
+                    iterator.remove();
+                }else if (oneZosa.contains(str.substring(str.length()-1))){
+                    iterator.remove();
                 }
             }
         }
@@ -130,21 +149,21 @@ public class HelpDeskKeyword {
         return distinctWordSet;
     }
 
-    public static List<String> create7ColumnList(List<List<String>> list){
+
+    public static List<String> createColumnList(List<List<String>> list,int column){
         List<String> columnList = list.stream()
-                .map(it -> it.get(7))
+                .map(it -> it.get(column))
                 .collect(Collectors.toList());
 
-        //list.stream().map(it -> it.get(7)).forEach(System.out::println);
         return columnList;
     }
 
-    public static void checkSimilarity(List<String> columnList, Set<String> distinctWordSet){
+    public static List<Map.Entry<String, Double>> checkSimilarity(List<String> columnList, Set<String> distinctWordSet){
 
-        Map<String,Integer> wordSimilarityMap = new HashMap<>();
+        Map<String,Double> wordSimilarityMap = new HashMap<>();
 
         for (String str : distinctWordSet) {
-            int count = 1;
+            double count = 1;
 
             for (int i = 0; i < columnList.size(); i++) {
                 if (columnList.get(i).contains(str)){
@@ -152,26 +171,56 @@ public class HelpDeskKeyword {
                     if(wordSimilarityMap.get(str)!=null){
                         count++;
                     }
-                    wordSimilarityMap.put(str,count);
+
+                    double result = Math.round(count/ data.size() * 100)/100.0;
+
+                    wordSimilarityMap.put(str, result);
                 }
             }
         }
 
-        //정렬후 list 담기
-        wordSimilarityMap.entrySet().stream()
-                .sorted(Comparator.comparing(Map.Entry::getKey))
-                //.map(it->it.getKey())
-                //.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                .forEach(System.out::println);
+        List<Map.Entry<String,Double>> similarityWordList = wordSimilarityMap.entrySet().stream()
+                //.sorted(Comparator.comparing(Map.Entry::getKey)) // 단어기준 정렬
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue())) // 유사도기준 정렬
+                .collect(Collectors.toList());
 
+        return similarityWordList;
     }
 
+    public static Map<String,Double> detailSimilarity(List<String> columnList, List<Map.Entry<String,Double>> similarityWordList){
+        Map<String, Double> detailMap = new HashMap<>();
+        for (int i = 0; i < 20; i++) {
+            Set<String> set = detailDistinctWordSet(columnList,similarityWordList.get(i).getKey());
+
+            List<Map.Entry<String,Double>> list = checkSimilarity(columnList,set);
+
+            System.out.println("===============================");
+            list.stream().forEach(System.out::println);
+            /*for (int j = 0; j < list.size(); j++) {
+                if (detailMap.get(list.get(j).getKey())!=null){
+                    double beforeValue = detailMap.get(list.get(j).getKey());
+                    double afterValue = list.get(j).getValue();
+
+                    double finalValue = beforeValue + afterValue;
+
+                    detailMap.put(list.get(j).getKey(),finalValue);
+                }else {
+                    double value = Math.round(list.get(j).getValue()*similarityWordList.get(i).getValue() * 100)/100.0;
+                    detailMap.put(list.get(j).getKey(),value);
+                }
+            }*/
+        }
+
+        return detailMap;
+    }
+
+
+
+    //2차 수정 logic 인데 걍 버릴거임 ㅋㅋ
     @Deprecated
     public static List<String> makeDictionary(Set<String> distinctWordSet){
         List<String> includedList = distinctWordSet.stream()
-                //.sorted(Comparator.comparing(String::new))
                 .collect(Collectors.toList());
-
         for (String strSet : distinctWordSet){
             for (int i =0 ;i<includedList.size();i++){
                 if (!strSet.equals(includedList.get(i)) && includedList.get(i).contains(strSet)){
@@ -179,7 +228,6 @@ public class HelpDeskKeyword {
                 }
             }
         }
-
         return includedList;
     }
 
