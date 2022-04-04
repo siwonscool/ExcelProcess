2897d362fef9bd202b3699430c3166ea13717fe7
김시원 <kimsea13@naver.com>
2022-04-04 18:04:58 +0900

    helpdesk 키워드 추출 5차 완료

--- src/com/company/HelpDeskKeyword.java
+++ src/com/company/HelpDeskKeyword.java
@@ -10,32 +10,40 @@ import java.util.stream.Collectors;
 
 public class HelpDeskKeyword {
     public static String path = "C:\\Users\\urp시스템\\Desktop\\helpDesk 분석\\";	//파일 경로 설정
-    public static String filename = "HelpDesk 분석_2019_2022_0310.xlsx";	//파일명 설정
+    public static String filename = "연도별 분할 데이터.xlsx";	//파일명 설정
 
-    public static List<List<String>> data = readExcel(path,filename);
+    public static List<List<String>> data = readExcel(path,filename,2);
 
     public static void main(String[] args) {
-        List<String> column7List =createColumnList(data,7);
+        List<String> column7List = createColumnList(data,7);
 
         List<Map.Entry<String, Double>> similarityList = checkSimilarity(column7List, initDistinctWordSet(column7List));
 
         Map<String,Double> detailOneSimilarityMap = detailSimilarity(column7List,similarityList);
+        List<Map.Entry<String,Double>> similarity2List = detailOneSimilarityMap.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(Collectors.toList());
 
-        //Map<String,Double> detailTwoSimilarityMap = detailSimilarity(column7List,detailOneSimilarityMap.entrySet().stream().collect(Collectors.toList()));
+        Map<String,Double> detailTwoSimilarityMap = detailSimilarity(column7List,similarity2List);
+        List<Map.Entry<String,Double>> similarity3List = detailTwoSimilarityMap.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(Collectors.toList());
 
-        detailOneSimilarityMap.entrySet().stream()
+        Map<String,Double> detailThreeSimilarityMap = detailSimilarity(column7List,similarity3List);
+
+        detailThreeSimilarityMap.entrySet().stream()
                 .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
                 .forEach(System.out::println);
 
     }
 
-    public static List<List<String>> readExcel(String path,String filename) {
+    public static List<List<String>> readExcel(String path,String filename,int sheetIndex) {
         List<List<String>> list = new ArrayList<List<String>>();
 
         try {
             FileInputStream fi = new FileInputStream(path+filename);
             XSSFWorkbook workbook = new XSSFWorkbook(fi);
-            XSSFSheet sheet = workbook.getSheetAt(1);
+            XSSFSheet sheet = workbook.getSheetAt(sheetIndex);
 
             for(int i=1; i<sheet.getLastRowNum() + 1; i++) {
                 XSSFRow row = sheet.getRow(i);
@@ -111,7 +119,7 @@ public class HelpDeskKeyword {
     public static Set<String> removeZosa(Set<String> distinctWordSet){
 
         String oneZosa ="이/가/을/를/에/는/은";
-        String twoZosa ="이나/이고/이라/이랑/에서/가면/에서/에게/한테/으로/않음/안됨/되지/";
+        String twoZosa ="이나/이고/이라/이랑/에서/가면/에서/에게/한테/으로/않음/안됨/되지/인데";
         String threeZosa ="이라고/이라는/에서는/에서만/에서는/에서도/한테는/한테도/한테만/됩니다/";
         String fourZosa="않습니다/나옵니다/";
 
@@ -177,7 +185,8 @@ public class HelpDeskKeyword {
                         count++;
                     }
 
-                    double result = Math.round(count/ data.size() * 1000)/1000.0;
+                    //double result = Math.round(count/ data.size() * 1000)/1000.0;
+                    double result = count/ data.size();
 
                     wordSimilarityMap.put(str, result);
                 }
@@ -192,16 +201,15 @@ public class HelpDeskKeyword {
         return similarityWordList;
     }
 
-    public static Map<String,Double> detailSimilarity(List<String> columnList, List<Map.Entry<String,Double>> similarityWordList){
+    public static Map<String,Double> detailSimilarity(List<String> columnList, List<Map.Entry<String,Double>> beforeSimilarityWordList){
         Map<String, Double> detailMap = new HashMap<>();
 
         //상위 20개의 키워드만 가지고 연산
-        for (int i = 0; i < 20; i++) {
-            Set<String> set = detailDistinctWordSet(columnList,similarityWordList.get(i).getKey());
+        for (int i = 0; i < beforeSimilarityWordList.size(); i++) {
+            Set<String> set = detailDistinctWordSet(columnList,beforeSimilarityWordList.get(i).getKey());
+            Double wordCount = CountWordInExcel(beforeSimilarityWordList.get(i).getKey(),columnList);
 
-
-
-            List<Map.Entry<String,Double>> list = checkDetailSimilarity(columnList,set,similarityWordList.get(i).getValue());
+            List<Map.Entry<String,Double>> list = checkDetailSimilarity(columnList,set,wordCount);
 
             for (int j = 0; j < list.size(); j++) {
                 if (detailMap.get(list.get(j).getKey()) != null){
@@ -212,30 +220,27 @@ public class HelpDeskKeyword {
 
                     detailMap.put(list.get(j).getKey(),finalValue);
                 }else {
-                    double value = Math.round(list.get(j).getValue() * similarityWordList.get(i).getValue() * 100)/100.0;
+                    //double value = Math.round(list.get(j).getValue() * beforeSimilarityWordList.get(i).getValue() * 1000)/1000.0;
+                    double value = list.get(j).getValue() * beforeSimilarityWordList.get(i).getValue();
                     detailMap.put(list.get(j).getKey(),value);
                 }
             }
-            //ㅅㅂ ㅈ망
-            /*for (int j = 0; j < list.size(); j++) {
-                if (detailMap.get(list.get(j).getKey())!=null){
-                    double beforeValue = detailMap.get(list.get(j).getKey());
-                    double afterValue = list.get(j).getValue();
-
-                    double finalValue = beforeValue + afterValue;
-
-                    detailMap.put(list.get(j).getKey(),finalValue);
-                }else {
-                    double value = Math.round(list.get(j).getValue()*similarityWordList.get(i).getValue() * 100)/100.0;
-                    detailMap.put(list.get(j).getKey(),value);
-                }
-            }*/
         }
 
         return detailMap;
     }
 
-    private static List<Map.Entry<String, Double>> checkDetailSimilarity(List<String> columnList, Set<String> detailDistinctWordSet, Double value) {
+    private static Double CountWordInExcel(String key, List<String> columnList) {
+        double count = 0;
+        for (int i = 0; i < columnList.size(); i++) {
+            if (columnList.get(i).contains(key));
+            count ++;
+        }
+
+        return count;
+    }
+
+    private static List<Map.Entry<String, Double>> checkDetailSimilarity(List<String> columnList, Set<String> detailDistinctWordSet, Double wordCount) {
         Map<String,Double> detailSimilarityMap = new HashMap<>();
 
         for (String str : detailDistinctWordSet) {
@@ -248,7 +253,8 @@ public class HelpDeskKeyword {
                         count++;
                     }
 
-                    double result = Math.round(count / (value * data.size())* 1000)/1000.0;
+                    //double result = Math.round(count / wordCount * 1000)/1000.0;
+                    double result = count / wordCount;
 
                     detailSimilarityMap.put(str, result);
                 }
