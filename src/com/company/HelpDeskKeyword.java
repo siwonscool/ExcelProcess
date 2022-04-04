cf2f4a5b259a83db7fa4b2dcb43f8f8b3761a638
김시원 <kimsea13@naver.com>
2022-04-04 11:06:39 +0900

    helpdesk 키워드 추출 4차 완료

--- src/com/company/HelpDeskKeyword.java
+++ src/com/company/HelpDeskKeyword.java
@@ -18,10 +18,14 @@ public class HelpDeskKeyword {
         List<String> column7List =createColumnList(data,7);
 
         List<Map.Entry<String, Double>> similarityList = checkSimilarity(column7List, initDistinctWordSet(column7List));
-        //similarityList.stream().forEach(System.out::println);
 
         Map<String,Double> detailOneSimilarityMap = detailSimilarity(column7List,similarityList);
-        detailOneSimilarityMap.entrySet().stream().forEach(System.out::println);
+
+        //Map<String,Double> detailTwoSimilarityMap = detailSimilarity(column7List,detailOneSimilarityMap.entrySet().stream().collect(Collectors.toList()));
+
+        detailOneSimilarityMap.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .forEach(System.out::println);
 
     }
 
@@ -99,13 +103,14 @@ public class HelpDeskKeyword {
                 .map(it -> it.replaceAll("[^\uAC00-\uD7A30-9a-zA-Z//>/]"," "))
                 .map(it -> it.trim())
                 .filter(it->it.contains(word))
+                .filter(it->!it.equals(word))
                 .collect(Collectors.toSet());
         return removeZosa(detailDistinctWordSet);
     }
 
     public static Set<String> removeZosa(Set<String> distinctWordSet){
 
-        String oneZosa ="이/가/을/를/에";
+        String oneZosa ="이/가/을/를/에/는/은";
         String twoZosa ="이나/이고/이라/이랑/에서/가면/에서/에게/한테/으로/않음/안됨/되지/";
         String threeZosa ="이라고/이라는/에서는/에서만/에서는/에서도/한테는/한테도/한테만/됩니다/";
         String fourZosa="않습니다/나옵니다/";
@@ -172,7 +177,7 @@ public class HelpDeskKeyword {
                         count++;
                     }
 
-                    double result = Math.round(count/ data.size() * 100)/100.0;
+                    double result = Math.round(count/ data.size() * 1000)/1000.0;
 
                     wordSimilarityMap.put(str, result);
                 }
@@ -189,13 +194,29 @@ public class HelpDeskKeyword {
 
     public static Map<String,Double> detailSimilarity(List<String> columnList, List<Map.Entry<String,Double>> similarityWordList){
         Map<String, Double> detailMap = new HashMap<>();
+
+        //상위 20개의 키워드만 가지고 연산
         for (int i = 0; i < 20; i++) {
             Set<String> set = detailDistinctWordSet(columnList,similarityWordList.get(i).getKey());
 
-            List<Map.Entry<String,Double>> list = checkSimilarity(columnList,set);
 
-            System.out.println("===============================");
-            list.stream().forEach(System.out::println);
+
+            List<Map.Entry<String,Double>> list = checkDetailSimilarity(columnList,set,similarityWordList.get(i).getValue());
+
+            for (int j = 0; j < list.size(); j++) {
+                if (detailMap.get(list.get(j).getKey()) != null){
+                    double beforeValue = detailMap.get(list.get(j).getKey());
+                    double afterValue = list.get(j).getValue();
+
+                    double finalValue = beforeValue + afterValue;
+
+                    detailMap.put(list.get(j).getKey(),finalValue);
+                }else {
+                    double value = Math.round(list.get(j).getValue() * similarityWordList.get(i).getValue() * 100)/100.0;
+                    detailMap.put(list.get(j).getKey(),value);
+                }
+            }
+            //ㅅㅂ ㅈ망
             /*for (int j = 0; j < list.size(); j++) {
                 if (detailMap.get(list.get(j).getKey())!=null){
                     double beforeValue = detailMap.get(list.get(j).getKey());
@@ -214,6 +235,30 @@ public class HelpDeskKeyword {
         return detailMap;
     }
 
+    private static List<Map.Entry<String, Double>> checkDetailSimilarity(List<String> columnList, Set<String> detailDistinctWordSet, Double value) {
+        Map<String,Double> detailSimilarityMap = new HashMap<>();
+
+        for (String str : detailDistinctWordSet) {
+            double count = 1;
+
+            for (int i = 0; i < columnList.size(); i++) {
+                if (columnList.get(i).contains(str)){
+
+                    if(detailSimilarityMap.get(str)!=null){
+                        count++;
+                    }
+
+                    double result = Math.round(count / (value * data.size())* 1000)/1000.0;
+
+                    detailSimilarityMap.put(str, result);
+                }
+            }
+        }
+
+        return detailSimilarityMap.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(Collectors.toList());
+    }
 
 
     //2차 수정 logic 인데 걍 버릴거임 ㅋㅋ
